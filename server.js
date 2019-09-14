0566e9ef06bb6c4029abd07ead9abd20dfbd9c44
bruno <br_lima@live.com>
2019-09-14 01:16:13 -0300

    Versao

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-deploy'));
  
-//app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
  
-//res.sendFile(path.join(__dirname+'/angularapp/index.html'));
-//});
+    res.sendFile(path.join(__dirname+'/dist/angular-deploy/index.html'));
+
+});
  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3000 );
